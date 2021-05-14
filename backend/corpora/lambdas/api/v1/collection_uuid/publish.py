cf9ecda6e79611bb9dc122b031b03439028af7c6
Trent Smith <trent.smith@chanzuckerberg.com>
2021-05-14 13:04:39 -0700

    Block publish when there are tombstone or refreshed datasets

--- backend/corpora/lambdas/api/v1/collection_uuid/publish.py
+++ backend/corpora/lambdas/api/v1/collection_uuid/publish.py
@@ -2,7 +2,7 @@ from flask import make_response, g
 
 from .....common.corpora_orm import CollectionVisibility
 from .....common.entities import Collection
-from .....common.utils.exceptions import ForbiddenHTTPException
+from .....common.utils.exceptions import ForbiddenHTTPException, MethodNotAllowedException
 
 
 def post(collection_uuid: str, user: str):
@@ -10,5 +10,18 @@ def post(collection_uuid: str, user: str):
     collection = Collection.get_collection(db_session, collection_uuid, CollectionVisibility.PRIVATE, owner=user)
     if not collection:
         raise ForbiddenHTTPException()
+    if not publishable(collection):
+        raise MethodNotAllowedException("Unable to publish a revision with delete or revised datasets at this time.")
     collection.publish()
     return make_response({"collection_uuid": collection.id, "visibility": collection.visibility}, 202)
+
+
+def publishable(collection) -> bool:
+    """Check if the dataset can be publish. Remove this once tombstones and refresh datasets are supported"""
+    result = True
+    for dataset in collection.datasets:
+        if dataset.original_id:
+            if dataset.tombstone or not dataset.published:
+                result = False
+                break
+    return result
